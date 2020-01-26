5ca998555b8cf183a8c9324311e10ac77f566ef3
fizzyboo <munmridul@gmail.com>
2020-01-26 12:39:53 +0600

    initiated selenium .Need to change the path of chrome driver

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
 import requests
 import html5lib
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-URL = 'https://github.com/Mridul98/AI-Reference-Papers'
 
-req = requests.get(URL)
-bs = BeautifulSoup(req.content,features='html5lib')
-resultset = bs.find_all('a')
-counter = 0
-for links in resultset:
-    res = links['href']
+def mysb():
+    driver = webdriver.Chrome('C:\Users\GRANITE\Desktop\chromedriver.exe')
+    driver.get('www.facebook.com')
+    elem = driver.find_element_by_id('email')
+    elem.send_keys('munmridul@gmail.com')
+    elem.send_keys(Keys.RETURN)
+    assert "no results found " in driver.page_source
+    driver.close()
     
-    if( res.startswith('https') and res.endswith('.pdf')):
+
+
+
+def github():
+    URL = 'https://github.com/Mridul98/AI-Reference-Papers'
+    req = requests.get(URL)
+    bs = BeautifulSoup(req.content,features='html5lib')
+    resultset = bs.find_all('a')
+    counter = 0
+    for links in resultset:
+        res = links['href']
+    
+        if( res.startswith('https') and res.endswith('.pdf')):
     #    https://github.com/Mridul98/AI-Reference-Papers/raw/master/afl(1).pdf
-        res = res.replace('blob','raw')
-        res = res.replace('manjunath5496','Mridul98')
-        print(res)
-        dl = requests.get(res)
-        print(dl.headers)
-        # file = 'paper'+str(counter)+'.pdf'
-        # open(file,'wb').write(dl.content)
+            res = res.replace('blob','raw')
+            res = res.replace('manjunath5496','Mridul98')
+            print(res)
+            dl = requests.get(res)
+            print(dl.headers)
+            file = 'paper'+str(counter)+'.pdf'
+            open(file,'wb').write(dl.content)
         
         
-        # counter = counter+1
+            counter = counter+1
+            
+mysb()
+    
+
         
